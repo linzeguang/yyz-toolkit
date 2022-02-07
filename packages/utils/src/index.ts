41694bff2f860c0cd9c326e603d3af994edfc208
linzeguang <15817289730@163.com>
2022-02-07 16:38:08 +0800

    test

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,14 +1,14 @@
 export { initChain, getChain } from "./modules/chain";
 export { getBalance } from "./modules/getBalance";
-export { getLibrary } from "./modules/getLibrary";
+// export { getLibrary } from "./modules/getLibrary";
 export { getWeb3 } from "./modules/getWeb3";
 export { format } from "./modules/format";
-export {
-  connectors,
-  injected,
-  walletConnect,
-  ConnectorsByName,
-  POLLING_INTERVAL,
-  connectorLocalStorageKey,
-  walletLocalStorageKey,
-} from "./modules/connector";
+// export {
+//   connectors,
+//   injected,
+//   walletConnect,
+//   ConnectorsByName,
+//   POLLING_INTERVAL,
+//   connectorLocalStorageKey,
+//   walletLocalStorageKey,
+// } from "./modules/connector";
