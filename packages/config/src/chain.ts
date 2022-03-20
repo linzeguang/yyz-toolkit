7066984f90e44361a3ea292753820d28f946c8c5
林泽广 <15817289730@163.com>
2022-03-20 22:15:36 +0800

    修改gamechainrpc地址

--- packages/config/src/chain.ts
+++ packages/config/src/chain.ts
@@ -2,36 +2,22 @@ import { Chains, Chain } from "types";
 
 export enum CHAIN_IDS {
   GC_TEST = 735,
-  BSC_TEST = 97,
-  BSC = 56,
 }
 
 export const chains: Chains = {
   [CHAIN_IDS.GC_TEST]: {
-    chainBrowser: "https://43.132.183.20:4000/",
+    chainBrowser:
+      "http://ec2-13-251-38-31.ap-southeast-1.compute.amazonaws.com:4000",
     chainId: CHAIN_IDS.GC_TEST,
     chainName: "Gamechain test",
     simpleName: "GC_TEST",
-    chainRpc: "http://101.32.177.140:7345",
+    chainRpc:
+      "http://ec2-13-251-38-31.ap-southeast-1.compute.amazonaws.com:7345",
     nativeCurrency: {
       name: "GAM",
       symbol: "GAM",
       decimals: 18,
     },
-  },
-  [CHAIN_IDS.BSC_TEST]: {
-    chainBrowser: "https://testnet.bscscan.com/",
-    chainId: CHAIN_IDS.BSC_TEST,
-    chainName: "Binance Smart Chain Testnet",
-    simpleName: "bnb",
-    chainRpc: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-  },
-  [CHAIN_IDS.BSC]: {
-    chainBrowser: "https://bsc-dataseed1.ninicoin.io",
-    chainId: CHAIN_IDS.BSC,
-    chainName: "BNB Smart Chain Mainnet",
-    simpleName: "bnb",
-    chainRpc: "https://bscscan.com/",
   },
 };
 
--- packages/config/src/contracts.ts
+++ packages/config/src/contracts.ts
@@ -1,12 +1,13 @@
 import { Contracts } from "./types";
 import { CHAIN_IDS } from "./chain";
 
-const { GC_TEST, BSC_TEST, BSC } = CHAIN_IDS;
+const { GC_TEST } = CHAIN_IDS;
 
 export const contracts: Contracts = {
   node: {
     [GC_TEST]: "0x1111111111111111111111111111111111111111",
-    [BSC_TEST]: "",
-    [BSC]: "",
+  },
+  market: {
+    [GC_TEST]: "0x949F0D32ff605c4d4F6853116308C90e6d834d11",
   },
 };
